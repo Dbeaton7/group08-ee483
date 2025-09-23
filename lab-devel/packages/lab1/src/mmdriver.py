3957b360987215d0cbdb1e1b694e1edc0d5f9e26
Diego <Dragonuv69@gmail.com>
2025-09-23 10:28:32 -0400

    got it moving !

--- lab-devel/packages/lab1/src/mmdriver.py
+++ lab-devel/packages/lab1/src/mmdriver.py
@@ -7,9 +7,9 @@ class Driver():#CHANGE CLASSNAME to the name of your class
     def __init__(self):
         self.veh_name = os.environ['VEHICLE_NAME']
         self.pub = rospy.Publisher('/ee483mm08/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size = 10)
-        self.speed =  0
+        self.speed =  0.5
         self.state = 'forward'
-        turns = 0
+        self.turns = 0
         #self.subscriber = 'duckietown_msgs/WheelsCmdStamped'
             # USING PARAMETER TO GET THE NAME OF THE VEHICLE
             # THIS WILL BE USEFUL TO SPECIFY THE NAME OF THE TOPIC
@@ -17,45 +17,59 @@ class Driver():#CHANGE CLASSNAME to the name of your class
 
     def drive(self): # CHANGE TO THE NAME OF YOUR FUNCTION
         print("running function")
+        print("waiting to start....\n")
         cmd_to_publish = WheelsCmdStamped()
-        print(self.state)
+        print(self.state, self.speed)
 
-        try:
-            if self.state == 'forward':
-                cmd_to_publish.header.stamp = rospy.Time.now()
-                cmd_to_publish.vel_right = self.speed
-                cmd_to_publish.vel_left = self.speed
-                self.pub.publish(cmd_to_publish)
-                rospy.sleep(2)
-
-                self.state = 'turning'
-            
-            elif self.state == 'turning':
-                turns += 1
-                cmd_to_publish.header.stamp = rospy.Time.now()
-                cmd_to_publish.vel_right = 1
-                cmd_to_publish.vel_left = self.speed
-                self.pub.publish(cmd_to_publish)
-                rospy.sleep(1)
-
-                self.state = 'forward'
-                
-                if turns >= 4:
-                    self.state = 'stop'
-
-            elif self.state == 'stop':
-                cmd_to_publish.header.stamp = rospy.Time.now()
-                cmd_to_publish.vel_right = 0
-                cmd_to_publish.vel_left = 0
-                self.pub.publish(cmd_to_publish)
-                self.state = 'finished'
-
-        except:
+        #try:
+        if self.state == 'forward':
             cmd_to_publish.header.stamp = rospy.Time.now()
             cmd_to_publish.vel_right = 0
             cmd_to_publish.vel_left = 0
             self.pub.publish(cmd_to_publish)
+            rospy.sleep(2)
 
+            cmd_to_publish.header.stamp = rospy.Time.now()
+            cmd_to_publish.vel_right = self.speed
+            cmd_to_publish.vel_left = self.speed
+            self.pub.publish(cmd_to_publish)
+            rospy.sleep(2)
+
+            self.state = 'turning'
+        
+        elif self.state == 'turning':
+            self.turns += 1
+            cmd_to_publish.header.stamp = rospy.Time.now()
+            cmd_to_publish.vel_right = 0
+            cmd_to_publish.vel_left = 0
+            self.pub.publish(cmd_to_publish)
+            rospy.sleep(5)
+
+            cmd_to_publish.header.stamp = rospy.Time.now()
+            cmd_to_publish.vel_right = 0
+            cmd_to_publish.vel_left = 0.5
+            self.pub.publish(cmd_to_publish)
+            rospy.sleep(0.5)
+
+            self.state = 'forward'
+            
+            if self.turns >= 4:
+                self.state = 'stop'
+
+        elif self.state == 'stop':
+            cmd_to_publish.header.stamp = rospy.Time.now()
+            cmd_to_publish.vel_right = 0
+            cmd_to_publish.vel_left = 0
+            self.pub.publish(cmd_to_publish)
+            self.state = 'finished'
+'''
+        except:
+            rospy.loginfo("Except running...")
+            cmd_to_publish.header.stamp = rospy.Time.now()
+            cmd_to_publish.vel_right = 0
+            cmd_to_publish.vel_left = 0
+            self.pub.publish(cmd_to_publish)
+'''
 
 #WRITE THE CODE TO MAKE THE MM GO AROUND THE BLOCK
 if __name__ == "__main__": ## The main function which will be called when your python sc
@@ -63,7 +77,7 @@ if __name__ == "__main__": ## The main function which will be called when your p
     try:
         rospy.init_node('driving')
         drive = Driver() # Create obj of the Driver class
-        rospy.sleep(3) # Delay to wait enough time for the code to run
+        rospy.sleep(8) # Delay to wait enough time for the code to run
         # Keep the line above - you might be able to reduce the delay a bit,
         while not rospy.is_shutdown() and drive.state != 'finished': # Run ros forever - you can change
             # this as well instead of running forever
