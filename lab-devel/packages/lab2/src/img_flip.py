9ddfe5705cf4e5ad89c687e87e24c0d2d107ffea
Diego <Dragonuv69@gmail.com>
2025-10-16 10:28:56 -0400

    slow day... have to change the encoding of the images

--- lab-devel/packages/lab2/src/img_flip.py
+++ lab-devel/packages/lab2/src/img_flip.py
@@ -3,20 +3,21 @@
 import sys
 import rospy
 import cv2
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge 
 
 class ImageFlipper:
     def __init__(self):
         #Instatiate the converter class once by using a class member
         self.bridge = CvBridge()
-        rospy.Subscriber("image", Image, self.create_filter)
+        rospy.Subscriber("/ee483mm08/camera_node/image/compressed", CompressedImage, self.create_filter)
         self.pub_white = rospy.Publisher("white", Image, queue_size=10)
         self.pub_yellow = rospy.Publisher("yellow", Image, queue_size=10)
 
     def flipper_cb(self,msg):
         #convert to a ROS image using the bridge
-        cv_img = self.bridge.imgmsg_to_cv2(msg,"bgr8")
+        
+        cv_img = self.bridge.compressed_imgmsg_to_cv2(msg,"bgr8")
 
         #flip along the horizontal axis using an OpenCV function
         cv_flipped = cv2.flip(cv_img, 1)
@@ -28,23 +29,23 @@ class ImageFlipper:
         self.pub.publish(ros_flipped)
 
     def create_filter(self, msg):
-
+        print("I got here")
         #convert to a ROS image using the bridge
-        cv_img = self.bridge.imgmsg_to_cv2(msg,"brg8")
+        cv_img = self.bridge.compressed_imgmsg_to_cv2(msg)
         hsv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2HSV)
 
         # create the yellow mask
-        cv_mask_yellow = cv2.inRange(hsv, (20,58,126), (30,143,255))
+        cv_mask_yellow = cv2.inRange(hsv_img, (0,103,151), (59,255,255))
 
         # create the white mask
-        cv_mask_white = cv2.inRange(hsv, (0,0,132), (168,23,194))
+        cv_mask_white = cv2.inRange(hsv_img, (0,0,219), (180,70,255))
 
-        masked_yellow = cv2.bitwise_and(hsv_img, hsv_img, cv_mask_yellow)
-        masked_white = cv2.bitwise_and(hsv_img, hsv_img, cv_mask_white)
+        # masked_yellow = cv2.bitwise_and(hsv_img, hsv_img, cv_mask_yellow)
+        # masked_white = cv2.bitwise_and(hsv_img, hsv_img, cv_mask_white)
 
         #convert new image to ROS to send
-        ros_masked_yellow = self.bridge.cv2_to_imgmsg(masked_yellow, "bgr8")
-        ros_masked_white = self.bridge.cv2_to_imgmsg(masked_white, "bgr8")
+        ros_masked_yellow = self.bridge.cv2_to_imgmsg(cv_mask_yellow, "bgr8")
+        ros_masked_white = self.bridge.cv2_to_imgmsg(cv_mask_white, "bgr8")
 
         #publish flipped image
         self.pub_white.publish(ros_masked_white)
